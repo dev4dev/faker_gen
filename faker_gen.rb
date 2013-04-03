f7f53b917dbfb3e9304d5c136a450609d632013e
Alex Antonyuk <alex@antonyuk.me>
2013-04-03 14:44:03 +0300

    added new generator types

--- faker_gen.rb
+++ faker_gen.rb
@@ -3,17 +3,43 @@ require "json"
 
 module FG
   class Obj
+    @@types = {
+      :person_name => ->{Faker::Name.name},
+      
+      :company_name => ->{Faker::Company.name},
+      :company_slogan => ->{Faker::Company.catch_phrase},
+      
+      :address => ->{Faker::Address.street_address(true)},
+      :address_country => ->{Faker::Address.country},
+      :address_city => ->{Faker::Address.city},
+      :address_zip => ->{Faker::Address.zip_code},
+      :address_street => ->{Faker::Address.street_name},
+      
+      :phone => ->{Faker::PhoneNumber.phone_number},
+      
+      :internet_domain => ->{Faker::Internet.domain_name},
+      :internet_email => ->{Faker::Internet.email},
+      :internet_ip4 => ->{Faker::Internet.ip_v4_address},
+      :internet_ip6 => ->{Faker::Internet.ip_v6_address},
+      :internet_url => ->{Faker::Internet.url},
+      :internet_username => ->{Faker::Internet.user_name},
+      
+      :lorem_word => ->{Faker::Lorem.word},
+      :lorem_sentence => ->{Faker::Lorem.sentence},
+      :lorem_paragraph => ->{Faker::Lorem.paragraph},
+    }
+    
     def initialize &block
-      @types = {
-        :person_name => ->{Faker::Name.name},
-        :company_name => ->{Faker::Company.name}
-      }
       @props = {}
       instance_eval &block
     end
     
     def prop name, type = :person_name
-      @props[name] = !@types[type].nil? ? @types[type] : type
+      @props[name] = !@@types[type].nil? ? @@types[type] : type
+    end
+    
+    def self.types
+      @@types.keys
     end
     
     def to_json *a
--- run.rb
+++ run.rb
@@ -1,9 +1,13 @@
 require_relative "faker_gen"
 
+puts FG::Obj.types
+
 data1 = FG::generate do
   prop 'owner'
   prop 'ceo', :person_name
   prop 'company', :company_name
+  prop 'address', :address
+  prop 'phone', :phone
 end
 
 puts data1
@@ -12,6 +16,7 @@ data2 = FG::generate 3 do
   prop 'name'
   prop 'image', 'http://lorempixel.com/124/124'
   prop 'size', 10
+  prop 'ok', :lorem_paragraph
 end
 
 puts data2
